a494ca53f3c0d25de93fc2ed09443bf11651be6f
Andres Paternina <eapaternina@uninorte.edu.co>
2022-04-25 22:55:52 -0500

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        char letra = 'p';
+        String cadena = "Prueba";
+        byte maspequeño = 127;
+        short pequeño = 2;
+        int entero = 1;
+        long otro = 5;
+        boolean isNumero = true;
+        float numero = 0.855464564f;
+        double numero2 = 0.855464564;
+        final float HORAS = 24;
+        int dias = 10, horas;
+        System.out.println("El numero de horas es "+ (dias*HORAS));
+        int dividendo = 5;
+        Integer divisor=3;
+        float division = dividendo/divisor;
+        System.out.println("La division es: " + division);
+        String hola= "hola", mundo = "mundo";
+        String cadena3 = hola + " " +mundo;
+        System.out.println(cadena3);
+
+        Scanner scanner = new Scanner(System. in);
+        System.out.println("Ingresa el primer numero a sumar: ");
+        String cadena_numero_1 = scanner.nextLine();
+        Integer numero1 = Integer.parseInt(cadena_numero_1);
+        System.out.println("Ingresa el segundo numero a sumar: ");
+        String cadena_numero_2 = scanner.nextLine();
+        Integer numero2 = Integer.parseInt(cadena_numero_2);
+        System.out.println("La suma es: " + (numero1 + numero2));
+        .*/
+
+        Scanner scanner = new Scanner(System. in);
+        System.out.println("Ingresa el primer numero a sumar: ");
+        Integer numero1 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Ingresa el segundo numero a sumar: ");
+        Integer numero2 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("La suma es: " + (numero1 + numero2));
+
+
+
+
+
+
+    }
+}
+
+
